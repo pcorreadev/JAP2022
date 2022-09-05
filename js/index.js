b954bd3ec62708b123c48f01a2f9c4f886bf8b4a
Pablo N. Correa <71953662+pcorreadev@users.noreply.github.com>
2022-09-04 23:47:08 -0300

    se agregaron funcionalidades de filter y sort

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
     let usuario = localStorage.getItem('user');
+
     if (usuario === null){
         window.location = "login.html"
     } else {
     document.getElementById('user').innerHTML = usuario ;
+    }; 
+
     document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
+        let category = localStorage.getItem('id')
+        localStorage.setItem("catID", category)
         window.location = "products.html"
     });
+
     document.getElementById("juguetes").addEventListener("click", function() {
         localStorage.setItem("catID", 102);
         window.location = "products.html"
@@ -17,6 +21,4 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     })
-}});
-
-
+})
--- js/products.js
+++ js/products.js
@@ -1,34 +1,81 @@
+let minCount = undefined;
+let maxCount = undefined;
+let currentArray = []
+
 const getProducts = () => {
-    fetch('https://japceibal.github.io/emercado-api/cats_products/101.json').then((data)=>{
-    return data.json();
-   
-    }).then((data)=> {
         let product="";
-        data.products.map((item)=>{
-            product+=`
-            <li class="list-group-item">
-            <div class="media align-items-lg-center flex-column flex-lg-row p-3">
-                <div>
-                    <h3 class="mt-0 font-weight-bold mb-2">${item.name}</h3>
-                    <p class="font-italic text-muted mb-0 small">${item.description}</p>
-                    <div class="d-flex align-items-center justify-content-between mt-1">
-                        <h4 class="font-weight-bold my-2"> ${item.currency + " " + item.cost }</h4>
-                        <h4 class="font-weight-bold my-2"> ${item.soldCount + " vendidos"}</h4>
+
+        currentArray.products.map((item)=>{
+                if (((minCount == undefined) || (minCount != undefined && item.cost >= minCount)) &&
+                ((maxCount == undefined) || (maxCount != undefined && item.cost <= maxCount))) {
+                product +=`
+                <li class="list-group-item">
+                <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div>
+                        <h3 class="mt-0 font-weight-bold mb-2">${item.name}</h3>
+                        <p class="font-italic text-muted mb-0 small">${item.description}</p>
+                        <div class="d-flex align-items-center justify-content-between mt-1">
+                            <h4 class="font-weight-bold my-2"> ${item.currency + " " + item.cost }</h4>
+                            <h4 class="font-weight-bold my-2"> ${item.soldCount + " vendidos"}</h4>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <img src="${item.image}" alt="item image" width="200" class media align-items-lg-left>
-            </div>`
-        });
-        document.getElementById('product list').innerHTML= product;
-        document.getElementById('categoria').innerHTML += data.catName;
+                <div>
+                    <img src="${item.image}" alt="item image" width="200" class media align-items-lg-left>
+                </div>`
+               }
+            })
+            document.getElementById('product list').innerHTML= product    
+        }
 
-    })
-    .catch ((error)=>{
-        console.log(error);
+document.getElementById("filtrar").addEventListener("click", function(){
+    minCount = document.getElementById("min").value;
+    maxCount = document.getElementById("max").value;
+    console.log(maxCount)
+    getProducts();
+});
+      
+document.getElementById("reset").addEventListener("click", function(){
+    getJSONData('https://japceibal.github.io/emercado-api/cats_products/101.json').then(function(resultObj){
+        currentArray = resultObj.data 
+        minCount = document.getElementById("min").value = '';
+        maxCount = document.getElementById("max").value = '';
+        getProducts();
     });
+})
 
-}
+document.getElementById("sortRelev").addEventListener("click", function () {
+    currentArray.products.sort(function (a, b) {
+        return b.soldCount - a.soldCount 
+    })
+    getProducts()
+});
 
-getProducts();
+document.getElementById("sortAsc").addEventListener("click", function () {
+    currentArray.products.sort(function (a, b) {
+        return a.cost - b.cost
+    })
+    getProducts()
+});
+
+document.getElementById("sortDesc").addEventListener("click", function () {
+    currentArray.products.sort(function (a, b) {
+       return b.cost - a.cost
+     })
+    getProducts()
+ });
+
+document.addEventListener("DOMContentLoaded", function(e){
+    let usuario = localStorage.getItem('user');
+    if (usuario === null){
+        window.location = "login.html"
+    } else {
+    document.getElementById('user').innerHTML = usuario ;
+    }
+    getJSONData('https://japceibal.github.io/emercado-api/cats_products/101.json').then(function(resultObj){
+            currentArray = resultObj.data
+            document.getElementById('categoria').innerHTML += currentArray.catName;  
+            getProducts();
+    });
+});
+ 
